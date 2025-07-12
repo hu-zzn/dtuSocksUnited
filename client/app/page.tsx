cf450505a9ac55f627d8f9d570d62982b31e4f3c
Hussain Abbas Zaidi <hussainabbaszaidi7@gmail.com>
2025-07-12 23:55:23 +0530

    dark mode

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,10 +3,10 @@ import { Hero } from "../components/hero"
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 text-black dark:bg-black dark:text-white">
       <Hero />
       
-      <section className="py-16">
+      <section className="py-16 bg-white dark:bg-black transition-colors">
         <div className="container mx-auto px-4 scale-[0.90] origin-top">
           <SocietyGrid />
         </div>
