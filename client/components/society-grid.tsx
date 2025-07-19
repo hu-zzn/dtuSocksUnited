9140ba58c6dfbe090476dba54d8d513bf674e423
Hussain Abbas Zaidi <hussainabbaszaidi7@gmail.com>
2025-07-19 15:34:13 +0530

    speed of scrolling

--- client/components/society-grid.tsx
+++ client/components/society-grid.tsx
@@ -191,7 +191,7 @@ export function SocietyGrid() {
                 grabCursor={true}
                 touchRatio={1}
                 loop={true}
-                speed={222}
+                speed={123}
                 spaceBetween={24}
                 slidesPerView="auto"
                 breakpoints={{
