9e5f909976ce8a669c9d0e4429f1dbc3a7855ca7
Hussain Abbas Zaidi <hussainabbaszaidi7@gmail.com>
2025-08-05 14:39:26 +0530

    Update page.tsx

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,6 +1,8 @@
 // client/app/login/page.tsx
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
