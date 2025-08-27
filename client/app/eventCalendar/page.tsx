6fb6844faa073c9e51f032b2effbac98bf3499a1
Ashish <kumarashish91011@gmail.com>
2025-08-27 22:51:14 +0530

    push2

--- client/app/eventCalendar/page.tsx
+++ client/app/eventCalendar/page.tsx
@@ -27,6 +27,7 @@ const App = () => {
     { societyName: "SAHITYA", eventDate: "21-08-2025", venue: "CONVOCATION HALL", time: "3:00 pm", isNew: true },
     { societyName: "COGNITIVE MINDS", eventDate: "20-08-2025", venue: "AB3 218", time: "4:00 pm", isNew: true },
     { societyName: "DTU Bhangra", eventDate: "27-08-2025", venue: "Windpoint (Science Block)", time: "3:30 pm", isNew: true },
+     { societyName: "DelTech MUN and Debsoc ", eventDate: "28-08-2025", venue: "AB", time: "12:00 pm", isNew: true },
 
     
   ];
